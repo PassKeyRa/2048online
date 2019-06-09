ec0a16b187d53bfee185efa3282290ebeab9f84d
PassKeyRa <posikera@bk.ru>
2019-06-09 13:33:29 +0300

    Changed board style and size

--- static/game.js
+++ static/game.js
@@ -5,21 +5,19 @@ function getRandomInt(min, max){
 }
 
 var board = [
-	0,0,0,0,0,0,
-	0,0,0,0,0,0,
-	0,0,0,0,0,0,
-	0,0,0,0,0,0,
-	0,0,0,0,0,0,
-	0,0,0,0,0,0
+	0,0,0,0,
+	0,0,0,0,
+	0,0,0,0,
+	0,0,0,0
 ];
 
 function check_lose(){
-	for (var i = 0; i < 35; i++){
-		if (i % 6 != 5 && board[i] == board[i+1]){
+	for (var i = 0; i < 15; i++){
+		if (i % 4 != 3 && board[i] == board[i+1]){
 			return true;
 		}
-		if (i < 30){
-			if (board[i] == board[i+6]){
+		if (i < 12){
+			if (board[i] == board[i+4]){
 				return true;
 			}
 		}
@@ -43,32 +41,37 @@ function move(capt){
 }
 
 var canvas = document.getElementById('canvas');
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 475;
+canvas.height = 475;
 var ctx = canvas.getContext('2d');
 
+function draw_board(){
+	for (var i = 0; i < 16; i++){
+		var x = ((i%4)*100)+(((i%4)+1)*15);
+		var y = (Math.floor(i/4)*100) + ((Math.floor(i/4)+1)*15);
+		ctx.beginPath();
+		ctx.fillStyle = "#a59d95";
+		ctx.arc(x+5, y+5, 5, 0, Math.PI*2, false);
+		ctx.fill();
+		ctx.fillRect(x+5, y, 90, 10);
+		ctx.arc(x+95, y+5, 5, 0, Math.PI*2, false);
+		ctx.fill();
+		ctx.fillRect(x+90, y+5, 10, 90);
+		ctx.arc(x+95, y+95, 5, 0, Math.PI*2, false);
+		ctx.fillRect(x+5, y+90, 90, 10);
+		ctx.arc(x+5, y+95, 5, 0, Math.PI*2, false);
+		ctx.fill();
+		ctx.fillRect(x, y+5, 10, 90);
+	}
+}
+
 function draw_block(k){
-	var x = (k%6)*100;
-	var y = Math.floor(k/6)*100;
-	ctx.beginPath();
-	ctx.fillStyle = "#afaa93";
-	ctx.arc(x+6, y+6, 5, 0, Math.PI*2, false);
-	ctx.fill();
-	ctx.fillRect(x+6, y+1, 88, 10);
-	ctx.arc(x+93, y+6, 5, 0, Math.PI*2, false);
-	ctx.fill();
-	ctx.fillRect(x+88, y+6, 10, 88);
-	ctx.arc(x+93, y+93, 5, 0, Math.PI*2, false);
-	ctx.fill();
-	ctx.fillRect(x+6, y+88, 88, 10);
-	ctx.arc(x+6, y+93, 5, 0, Math.PI*2, false);
-	ctx.fill();
-	ctx.fillRect(x+1, y+6, 10, 88);
+	
 }
 
 function new_block(){
 	var spaces = [];
-	for (var i = 0; i < 36; i++){
+	for (var i = 0; i < 16; i++){
 		if (board[i] == 0){
 			spaces.push(i);
 		}
@@ -78,8 +81,10 @@ function new_block(){
 	draw_block(tmp);
 }
 
+draw_board();
+
 function start(){
-	draw_block(0);
+	/*draw_block(0);
 	draw_block(1);
-	draw_block(6);
+	draw_block(6);*/
 }
