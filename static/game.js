252fc8dff98ab20edaca4038ef695bd1d500cc07
PassKeyRa <posikera@bk.ru>
2019-06-13 21:22:27 +0300

    Nicht arbeitet.

--- static/game.js
+++ static/game.js
@@ -7,10 +7,10 @@ function getRandomInt(min, max){
 }
 
 var board = [
-	2,4,8,16,
-	32,64,128,256,
-	512,1024,2048,4096,
-	8192,0,0,0
+	0,2,0,2,
+	0,0,2,0,
+	0,0,4,8,
+	0,0,0,0
 ];
 
 function check_lose(){
@@ -25,21 +25,6 @@ function check_lose(){
 		}
 	}
 	return false;
-}
-
-function move(capt){
-	if (capt == 'left'){
-
-	}
-	if (capt == 'right'){
-
-	}
-	if (capt == 'up'){
-
-	}
-	if (capt == 'down'){
-
-	}
 }
 
 var canvas = document.getElementById('canvas');
@@ -105,6 +90,46 @@ function draw_block(k){
 	}
 }
 
+function draw_block2(k, x, y){
+	var col =  colors[(Math.log2(board[k])-1)%12];
+	ctx.beginPath();
+	ctx.fillStyle = col;
+	if (board[k] <= 512){
+		ctx.font = "48px Helvetica";
+	}
+	else{
+		ctx.font = "40px Helvetica";
+	}
+	ctx.textAlign = "center";
+	ctx.arc(x+5, y+5, 5, 0, Math.PI*2, false);
+	ctx.fill();
+	ctx.fillRect(x+5, y, 90, 10);
+	ctx.arc(x+95, y+5, 5, 0, Math.PI*2, false);
+	ctx.fill();
+	ctx.fillRect(x+90, y+5, 10, 90);
+	ctx.arc(x+95, y+95, 5, 0, Math.PI*2, false);
+	ctx.fillRect(x+5, y+90, 90, 10);
+	ctx.arc(x+5, y+95, 5, 0, Math.PI*2, false);
+	ctx.fill();
+	ctx.fillRect(x, y+5, 10, 90);
+	if ((Math.log2(board[k])-1)%12 < 2){
+		ctx.fillStyle = "#3d3b39";
+	}
+	else{
+		ctx.fillStyle = "white";
+	}
+	if (board[k] <= 512){
+		ctx.fillText(board[k].toString(10), x+50, y+65, 80);
+	}
+	else{
+		ctx.fillText(board[k].toString(10), x+50, y+63, 80);
+	}
+}
+
+/*function clear_block(k, x, y){
+
+}*/
+
 function new_block(){
 	var spaces = [];
 	for (var i = 0; i < 16; i++){
@@ -119,47 +144,137 @@ function new_block(){
 
 draw_board();
 
+function move(capt){
+	if (capt == 'left'){
+		var speed = [];
+		for (var i = 0; i < board.length; i++){
+			if (board[i] == 0 || i % 4 == 0){
+				var x = ((i%4)*100)+(((i%4)+1)*15);
+				var y = (Math.floor(i/4)*100) + ((Math.floor(i/4)+1)*15);
+				if (i % 4 == 0 && board[i] != 0){
+					speed.push([0, x, y, x, y, true]);
+				}
+				else{
+					speed.push([0, x, y, x, y, false]);
+				}
+				console.log(0)
+				continue;
+			}
+			var tmp = i % 4;
+			var k = 0;
+			for (var j = 1; j <= tmp; j++){
+				if (board[i-j]!=0){
+					if (board[i-j]==board[i]){
+						k+=1;
+						k = -k;
+					}
+					break;
+				}
+				else{
+					k+=1;
+				}
+			}
+			var x = ((i%4)*100)+(((i%4)+1)*15);
+			var y = (Math.floor(i/4)*100) + ((Math.floor(i/4)+1)*15);
+			var x1 = (((i-Math.abs(k))%4)*100)+((((i-Math.abs(k))%4)+1)*15);
+			//console.log(k);
+			speed.push([k, x, y, x1, y, false]); //k, x, y, x1, y1
+		}
+		//console.log(speed);
+		
+		ctx.clearRect(0, 0, 475, 475);
+		draw_board();
+
+		for (var l = 0; l < speed.length; l++){
+			if (speed[l][0] == 0){
+				if (speed[l][5] == true){
+					draw_block2(l, speed[l][1], speed[l][2]);
+				}
+				continue;
+			}
+			
+			if (speed[l][1] != speed[l][3]){
+				if (speed[l][0] < 0){
+					board[l-Math.abs(speed[l][0])] = board[l]*2;
+					board[l] = 0;
+				}
+				else{
+					board[l-Math.abs(speed[l][0])] = board[l];
+					board[l] = 0;
+				}
+				speed[l][1] = speed[l][3];
+				draw_block2(l-Math.abs(speed[l][0]), speed[l][1], speed[l][2]);
+				continue;
+			}
+			/*if (speed[l][1] == speed[l][3] && speed[l][0] < 0){
+				board[l-Math.abs(speed[l][0])] += board[l];
+				board[l] = 0;
+				draw_block2(l-Math.abs(speed[l][0]), speed[l][1], speed[l][2]);
+				continue;
+			}*/
+		}
+		console.log(board);
+	}
+	/*if (capt == 'right'){
+		for (var i = 0; i < board.length; i++){
+			
+		}
+	}
+	if (capt == 'up'){
+		for (var i = 0; i < board.length; i++){
+			
+		}
+	}
+	if (capt == 'down'){
+		for (var i = 0; i < board.length; i++){
+			
+		}
+	}*/
+}
+
 function start(){
-	for (var i = 0; i < 13; i++){
-		draw_block(i);
+	for (var i = 0; i < board.length; i++){
+		if (board[i] != 0){
+			draw_block(i);
+		}
 	}
 	document.addEventListener('keydown', function(event){
 		switch (event.keyCode){
 			case 87:
 				move('up');
-				new_block();
+				//new_block();
 				break;
 			case 65:
 				move('left');
-				new_block();
+				//new_block();
 				break;
 			case 83:
 				move('down');
-				new_block();
+				//new_block();
 				break;
 			case 68:
 				move('right');
-				new_block();
+				//new_block();
 				break;
 		}
 
 		switch (event.keyCode){
 			case 38:
 				move('up');
-				new_block();
+				//new_block();
 				break;
 			case 37:
 				move('left');
-				new_block();
+				//new_block();
 				break;
 			case 40:
 				move('down');
-				new_block();
+				//new_block();
 				break;
 			case 39:
 				move('right');
-				new_block();
+				//new_block();
 				break;
 		}
-	}
+	});
 }
